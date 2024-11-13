4ac05fe628bc6ad745e3253abbecf93f74b669f5
Dare <42944223+MatyashDare@users.noreply.github.com>
2024-11-13 23:38:38 +0100

    evaluation_files

---
+++ evaluate_acc.py
@@ -0,0 +1,74 @@
+import os
+import json
+import argparse
+import warnings
+import pandas as pd
+
+warnings.filterwarnings('ignore')
+
+
+def get_acc_with_contion(res_pd, key, values):
+    if isinstance(values, list):
+        total_pd = res_pd[res_pd[key].isin(values)]
+    else:
+        total_pd = res_pd[res_pd[key] == values]
+    correct_pd = total_pd[total_pd['true_false'] == True]
+    if len(total_pd) == 0:
+        return -1
+    acc = "{:.2f}".format(len(correct_pd) / len(total_pd) * 100)
+    return acc
+
+
+def get_scores(res_pd):
+    num = res_pd.shape[0]
+    for index, row in res_pd.iterrows():
+        print(index)
+        res_pd.loc[index, 'no_context'] = True if (not row['hint'] and not row['image']) else False
+        res_pd.loc[index, 'has_text'] = True if row['hint'] else False
+        res_pd.loc[index, 'has_image'] = True if row['image'] else False
+        res_pd.loc[index, 'has_text_image'] = True if (row['hint'] and row['image']) else False
+
+        label = row['answer']
+        pred = row['answer_output']
+        res_pd.loc[index, 'pred'] = pred
+        res_pd.loc[index, 'true_false'] = (label == pred)
+
+    # accuracy scores
+    acc_average = len(res_pd[res_pd['true_false'] == True]) / num * 100
+    #assert result_file.split('_')[-1] == "{:.3f}.json".format(acc_average)
+
+    scores = {
+        'acc_natural':
+        get_acc_with_contion(res_pd, 'subject', 'natural science'),
+        'acc_social':
+        get_acc_with_contion(res_pd, 'subject', 'social science'),
+        'acc_language':
+        get_acc_with_contion(res_pd, 'subject', 'language science'),
+        'acc_has_text':
+        get_acc_with_contion(res_pd, 'has_text', True),
+        'acc_has_image':
+        get_acc_with_contion(res_pd, 'has_image', True),
+        'acc_no_context':
+        get_acc_with_contion(res_pd, 'no_context', True),
+        'acc_grade_1_6':
+        get_acc_with_contion(res_pd, 'grade', ['grade1', 'grade2', 'grade3', 'grade4', 'grade5', 'grade6']),
+        'acc_grade_7_12':
+        get_acc_with_contion(res_pd, 'grade', ['grade7', 'grade8', 'grade9', 'grade10', 'grade11', 'grade12']),
+        'acc_average':
+        "{:.2f}".format(acc_average),
+    }
+    topics = ['punctuation', 'literacy-in-science', 'verbs', 'pronouns', 'civics', 'culture', 'word-study', 'economics', 'physics', 'units-and-measurement', 'science-and-engineering-practices', 'reading-comprehension', 'global-studies', 'grammar', 'figurative-language', 'us-history', 'writing-strategies', 'world-history', 'reference-skills', 'biology', 'earth-science', 'phonological-awareness', 'capitalization', 'chemistry', 'vocabulary', 'geography']
+    for t in topics:
+        scores['acc_' + t] = get_acc_with_contion(res_pd, 'topic', t)
+
+    return scores
+
+
+def print_scores(scores):
+    latex_output = ""
+    for key, score in scores.items():
+        print(f"{key[4:]}: \t{score}")
+        latex_output += f"& {score} "
+    latex_output += "\\\\"
+    print(latex_output)
+
---
+++ evaluation.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import json, ast, re
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
+from evaluations import caculate_bleu, caculate_rouge, caculate_similariry
+from evaluate_acc import *
+
+
+def eval(RES_DIR:str, MODEL_NAME:str):
+    for i in range(1,5):
+        df = pd.read_csv(f"{RES_DIR}/{MODEL_NAME}_val_output_setting_{i}.csv", sep="\t")
+        df = pd.read_excel("../res/see.xlsx")
+        answer_pred_col, solution_pred_col = "answer_output", "solution_output"
+        df[[answer_pred_col, solution_pred_col]] = df["output"].apply(string2json).apply(pd.Series)
+        answer_preds = df[answer_pred_col].tolist()
+        solution_preds =  df[solution_pred_col].tolist()
+        scores = get_scores(df)
+        print_scores(scores)
+        metrics = calculate_metrics_solutions(solution_preds , df)
+        scores.update(metrics)
+        df_save = pd.DataFrame(scores)
+        df_save.to_csv(f"{RES_DIR}/{MODEL_NAME}_val_metrics.csv", sep="\t", encoding="utf-8")
+       
+
+def string2json(text):
+    # convert json-like string to "answer" and "solution"
+    dict_json = ast.literal_eval(re.sub("```|\n|json", "", text))
+    return dict_json["answer"], dict_json["solution"]
+
+
+def calculate_metrics_solutions(results, data):
+    ## BLEU
+    bleu1 = caculate_bleu(results, data, gram=1)
+    bleu4 = caculate_bleu(results, data, gram=4)
+    print("BLEU-1: %.3f" % bleu1)
+    print("BLEU-4: %.3f" % bleu4)
+
+    ## Rouge-L
+    rouge = caculate_rouge(results, data)
+    print("ROUGE-L: %.3f" % rouge)
+
+    ## Similarity
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2').cuda()
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    similarity = caculate_similariry(results, data, model)
+    print("Similariry: %.3f" % similarity)
+
+    return {"BLEU-1": [bleu1], "BLEU-4": [bleu4], "ROUGE": [rouge], "similarity": similarity}
