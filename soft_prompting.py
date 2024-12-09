03f5aa92065fb19b01745fb8fa19ca6364bc85b1
Florian Dreyer <flori.dreyer@gmail.com>
2024-12-09 17:10:55 +0100

    Soft prompting for decoder only

--- soft_prompting.py
+++ soft_prompting.py
@@ -18,7 +18,6 @@ class SoftPrompting:
             **kwargs,
     ):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model.to(device)
         for param in model.parameters():
             param.requires_grad = False
 
@@ -34,37 +33,23 @@ class SoftPrompting:
 
         return model
 
-    def set_soft_prompt_embeds(
-            self,
-            soft_prompt_path: str,
-    ) -> None:
-        """
-        Args:
-            soft_prompt_path: torch soft prompt file path
-
-        """
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.soft_prompt = torch.load(
-            soft_prompt_path, map_location=device
-        )
+    def set_soft_prompt_embeds(self, soft_prompt_path: str) -> None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.soft_prompt = torch.load(soft_prompt_path, map_location=device)
         self.n_tokens = self.soft_prompt.num_embeddings
         print(f"Set soft prompt! (n_tokens: {self.n_tokens})")
 
     def initialize_soft_prompt(
-            self,
-            n_tokens: int = 20,
-            initialize_from_vocab: bool = True,
-            random_range: float = 0.5,
+            self, n_tokens: int = 20, initialize_from_vocab: bool = True, random_range: float = 0.5
     ) -> None:
         self.n_tokens = n_tokens
         if initialize_from_vocab:
             init_prompt_value = self.transformer.wte.weight[:n_tokens].clone().detach()
         else:
-            init_prompt_value = torch.FloatTensor(2, 10).uniform_(
+            init_prompt_value = torch.FloatTensor(n_tokens, self.config.n_embd).uniform_(
                 -random_range, random_range
             )
         self.soft_prompt = nn.Embedding(n_tokens, self.config.n_embd)
-        # Initialize weight
         self.soft_prompt.weight = nn.parameter.Parameter(init_prompt_value)
 
     def _cat_learned_embedding_to_input(self, input_ids) -> torch.Tensor:
@@ -73,8 +58,9 @@ class SoftPrompting:
         if len(list(inputs_embeds.shape)) == 2:
             inputs_embeds = inputs_embeds.unsqueeze(0)
 
-        # [batch_size, n_tokens, n_embd]
-        learned_embeds = self.soft_prompt.weight.repeat(inputs_embeds.size(0), 1, 1)
+        learned_embeds = self.soft_prompt.weight.unsqueeze(0).expand(
+            inputs_embeds.size(0), -1, -1
+        )
 
         inputs_embeds = torch.cat([learned_embeds, inputs_embeds], dim=1)
 
@@ -87,57 +73,43 @@ class SoftPrompting:
         n_batches = labels.shape[0]
         return torch.cat(
             [
-                torch.full((n_batches, self.n_tokens), ignore_index).to(self.device),
+                torch.full((n_batches, self.n_tokens), ignore_index),
                 labels,
             ],
             dim=1,
         )
 
     def _extend_attention_mask(self, attention_mask):
-
         if len(list(attention_mask.shape)) == 1:
             attention_mask = attention_mask.unsqueeze(0)
 
         n_batches = attention_mask.shape[0]
         return torch.cat(
-            [torch.full((n_batches, self.n_tokens), 1).to(self.device), attention_mask],
+            [torch.full((n_batches, self.n_tokens), 1), attention_mask],
             dim=1,
         )
 
     def save_soft_prompt(self, path: str, filename: str = "soft_prompt.model"):
         Path(path).mkdir(parents=True, exist_ok=True)
         torch.save(self.soft_prompt, os.path.join(path, filename))
-        # print(f"Saved soft prompt: {os.path.join(path, filename)}")
 
     def forward(
             self,
             input_ids=None,
-            past_key_values=None,
             attention_mask=None,
-            token_type_ids=None,
-            position_ids=None,
-            head_mask=None,
-            inputs_embeds=None,
-            encoder_hidden_states=None,
-            encoder_attention_mask=None,
             labels=None,
             use_cache=None,
-            output_attentions=None,
-            output_hidden_states=None,
             return_dict=None,
     ):
         if input_ids is not None:
-            inputs_embeds = self._cat_learned_embedding_to_input(input_ids).to(
-                self.device
-            )
+            inputs_embeds = self._cat_learned_embedding_to_input(input_ids)
 
         if labels is not None:
-            labels = self._extend_labels(labels).to(self.device)
+            labels = self._extend_labels(labels)
 
         if attention_mask is not None:
-            attention_mask = self._extend_attention_mask(attention_mask).to(self.device)
+            attention_mask = self._extend_attention_mask(attention_mask)
 
-        # Drop most of the args for now
         return super().forward(
             attention_mask=attention_mask,
             inputs_embeds=inputs_embeds,
@@ -159,7 +131,7 @@ class MultimodalSoftPrompting(SoftPrompting):
     ):
         # Soft Prompting for texts
         if input_ids is not None:
-            inputs_embeds = self._cat_learned_embedding_to_input(input_ids).to(self.device)
+            inputs_embeds = self._cat_learned_embedding_to_input(input_ids)
 
         if images is not None:
             if isinstance(images, PIL.Image.Image):
@@ -168,14 +140,14 @@ class MultimodalSoftPrompting(SoftPrompting):
                     transforms.ToTensor(),
                     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
                 ])
-                images = transform(images).unsqueeze(0).to(self.device)
+                images = transform(images).unsqueeze(0)
 
             image_features = self.image_encoder(images)
 
         if labels is not None:
-            labels = self._extend_labels(labels).to(self.device)
+            labels = self._extend_labels(labels)
         if attention_mask is not None:
-            attention_mask = self._extend_attention_mask(attention_mask).to(self.device)
+            attention_mask = self._extend_attention_mask(attention_mask)
 
         multimodal_features = torch.cat([inputs_embeds, image_features], dim=1) # change to format for model
 
